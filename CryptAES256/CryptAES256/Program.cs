eda098d6497722169b9e03ad8c073c88a71c71bc
Fernando Pucci <fernando.pucci@consinco.com.br>
2017-04-28 15:56:09 -0300

    Commit Inicial

---
+++ CryptAES256/CryptAES256/Program.cs
@@ -0,0 +1,41 @@
+﻿using CryptAES256.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptAES256
+{
+    /// <summary>
+    /// Implement based on https://securityblog.gr/3340/aes-256-and-base64-using-cryptoapi-and-c-2/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var TEXT = new
+            {
+                Username = "jose.silva@testserver.com.br",
+                Password = "123456Test&"
+            };//"Valor a ser Decriptado";
+
+            Guid d = Guid.NewGuid();
+
+            string SHARED_KEY =  d.ToString(); 
+
+            String encryptedText = CryptUtils.Encrypt(TEXT.ToString(), SHARED_KEY);
+
+            Console.WriteLine(encryptedText);
+
+            Console.WriteLine("\n\n\n");
+
+            String decryptedText = CryptUtils.Decrypt(encryptedText, SHARED_KEY);
+            
+            Console.WriteLine(decryptedText);            
+
+        }
+    }
+}
